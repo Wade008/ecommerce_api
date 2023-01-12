d760d64ca148bff060237243ac9f3d5709b9a7c5
Wade Doolan <wadedoolan@hotmail.com>
2023-01-12 17:12:44 +1000

    Populate using virtual in cart

--- src/controllers/carts/cartRoutes.js
+++ src/controllers/carts/cartRoutes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 
-const { getCarts, getCartById, getCartByUserId } = require("./cartControllers")
+const { getCarts, getCartById, getCartByUserId,    getCartByUserIdWithProductInfo } = require("./cartControllers")
 
 const cartRouter = express.Router();
 
 
 
-cartRouter.get("/", (req, res) => {
-    const carts = getCarts()
+cartRouter.get("/", async (req, res) => {
+    const carts = await getCarts()
     res.json(carts)
 })
 
-cartRouter.get("/:cartId", (req, res) => {
+cartRouter.get("/:cartId", async (req, res) => {
 
-    const cart = getCartById(req.params.cartId)
+    const cart = await getCartById(req.params.cartId)
     if (!cart) {
         res.status(404).json({
             data: "Cart does not exist"
@@ -22,8 +22,14 @@ cartRouter.get("/:cartId", (req, res) => {
     res.json(cart)
 })
 
-cartRouter.get("/user/:userId", (request, response) => {
-    const cart = getCartByUserId(request.params.userId)
+// /carts/user/userId?getProductInfo=true
+cartRouter.get("/user/:userId", async (request, response) => {
+    let cart
+    if (request.query.getProductInfo) {
+        cart = await getCartByUserIdWithProductInfo(request.params.userId)
+    } else {
+        cart = await getCartByUserId(request.params.userId)
+    }
     if (!cart) {
         response.status(404).json({
             data: "cart doesn't exist",
@@ -31,6 +37,5 @@ cartRouter.get("/user/:userId", (request, response) => {
     }
     response.json(cart)
 })
-
 
 module.exports = cartRouter;
--- src/models/product.js
+++ src/models/product.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const ReviewSchema = new mongoose.Schema({
     user_id: String,
-    description: String
+    description: String,
 })
 
 const ProductSchema = new mongoose.Schema({
@@ -13,23 +13,23 @@ const ProductSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        validate: { //used for custom validation
+        validate: {
             validator: (value) => {
-                //supose description cannot contain word : "Hello"
+                //Suppose, decsription cannot contain the word "hello"
                 return !value.includes("hello")
             },
-            message: "Description cannot contain word : Hello the word 'hello'",
-        }
+            message: "Description cannot contain the word 'hello'",
+        },
     },
     price: {
         type: Number,
         required: true,
-        min: [0, "No negative price values"]
+        min: [0, "No Negative Price"],
     },
     stock: Number,
-    reviews: [ReviewSchema]
+    reviews: [ReviewSchema],
 })
 
-const Product = mongoose.model("product", ProductSchema);
+const Product = mongoose.model("Product", ProductSchema)
 
 module.exports = Product
